27af780535a8039ef33761c7515f7bc86ee9b743
SofiaRodas <sofiarodaso090907@gmail.com>
2025-03-27 15:30:40 -0500

    Par o Impar

---
+++ Tareas/Par o Impar/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Construir un programa que pida un número y luego diga si este número es par o no.
+
+do
+{
+    Console.Write("INGRESA UN NUMERO: ");
+    var numeroString = Console.ReadLine();
+    var numeroInt = int.Parse(numeroString);
+
+    if (numeroInt % 2 == 0)
+    {
+        Console.WriteLine("El numero es par");
+    }
+    else
+    {
+        Console.WriteLine("El numero es impar");
+    }
+} while (true);
