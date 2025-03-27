62efadf88951af4b77b4df3b46513c102b53a811
SofiaRodas <sofiarodaso090907@gmail.com>
2025-03-27 15:06:59 -0500

    Ejercicio 1

--- Algoritmos/Par o impar/Program.cs
+++ Algoritmos/Par o impar/Program.cs
@@ -1,8 +1,20 @@
+
 ﻿// Construir un programa que pida un número y luego diga si este número es par o no.
 
-Console.Write("Ingresa el primer numero: ");
-var numeroString = Console.ReadLine();
-var numeroInt = int.Parse(numeroString);
+do
+{
+    Console.Write("INGRESA UN NUMERO: ");
+    var numeroString = Console.ReadLine();
+    var numeroInt = int.Parse(numeroString);
 
+    if (numeroInt % 2 == 0)
+    {
+        Console.WriteLine("El numero es par");
+    }
+    else
+    {
+        Console.WriteLine("El numero es impar");
+    }
+} while (true);
 
 
