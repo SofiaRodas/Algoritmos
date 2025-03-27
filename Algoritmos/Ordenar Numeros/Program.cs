f0f0cd5a325180ca7cbc7d3606d42f7632ae0133
SofiaRodas <sofiarodaso090907@gmail.com>
2025-03-27 15:46:56 -0500

    tareas

---
+++ Algoritmos/Ordenar Numeros/Program.cs
@@ -0,0 +1,9 @@
+﻿// Construir un programa que pida por pantalla 3 números y luego diga cuál es el mayor, el del medio y el menor de los números ingresados.
+
+Console.WriteLine("INGRESE TRES NUMEROS DIFERENTES:");
+Console.Write("Ingrese primer numero:");
+var Numero1 = Console.ReadLine();
+Console.Write("Ingrese segundo numero:");
+var Numero2 = Console.ReadLine();
+Console.Write("Ingrese tercer numero:");
+var Numero3 = Console.ReadLine();
